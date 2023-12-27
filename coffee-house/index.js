648dfe149369431b55551f284e7e0c9031d557ef
gomunkool <pivzz19881988@gmil.com>
2023-12-27 23:06:48 +0100

    feat: start 3 part

--- coffee-house/index.js
+++ coffee-house/index.js
@@ -1,1 +1,10 @@
-console.log('all completed in accordance with the assignment, 100/100')
+//menu burger
+const menuBurger = document.querySelector('.header-burger')
+const menuBurgerLines = document.querySelector('.header-burger__lines')
+const headerNav = document.querySelector('.header-nav')
+
+menuBurger.addEventListener('click', () => {
+  menuBurgerLines.classList.toggle('header-burger__lines_open')
+  headerNav.classList.toggle('header-nav_open')
+
+})
