9afe7cd294e4291812c0e5905654bebe5e18b846
gomunkool <pivzz19881988@gmil.com>
2023-12-30 17:37:22 +0100

    feat: add burger-button

--- coffee-house/index.js
+++ coffee-house/index.js
@@ -1,10 +1,14 @@
 //menu burger
 const menuBurger = document.querySelector('.header-burger')
-const menuBurgerLines = document.querySelector('.header-burger__lines')
+const menuBurgerTopLine = document.querySelector('.header-burger__top-line')
+const menuBurgerBottomLine = document.querySelector('.header-burger__bottom-line')
 const headerNav = document.querySelector('.header-nav')
+const countNav = document.querySelector('.nav-count')
+
 
 menuBurger.addEventListener('click', () => {
-  menuBurgerLines.classList.toggle('header-burger__lines_open')
+  menuBurgerTopLine.classList.toggle('header-burger__top-line_open')
+  menuBurgerBottomLine.classList.toggle('header-burger__bottom-line_open')
   headerNav.classList.toggle('header-nav_open')
-
+  countNav.classList.toggle('nav-count_open')
 })
