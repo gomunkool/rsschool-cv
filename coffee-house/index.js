d1e23c193fbe1cf87950a340faf79f7f0de83e60
gomunkool <pivzz19881988@gmil.com>
2024-01-09 11:22:12 +0100

    feat: finish menu burger

--- coffee-house/index.js
+++ coffee-house/index.js
@@ -4,11 +4,30 @@ const menuBurgerTopLine = document.querySelector('.header-burger__top-line')
 const menuBurgerBottomLine = document.querySelector('.header-burger__bottom-line')
 const headerNav = document.querySelector('.header-nav')
 const countNav = document.querySelector('.nav-count')
+const burgerLinks = document.querySelectorAll('.header-nav li')
 
-
-menuBurger.addEventListener('click', () => {
+function openAndCloseItemMenuBurger() {
   menuBurgerTopLine.classList.toggle('header-burger__top-line_open')
   menuBurgerBottomLine.classList.toggle('header-burger__bottom-line_open')
   headerNav.classList.toggle('header-nav_open')
   countNav.classList.toggle('nav-count_open')
+}
+
+menuBurger.addEventListener('click', () => {
+  openAndCloseItemMenuBurger()
 })
+
+burgerLinks.forEach((item) => {
+  item.addEventListener('click', () => {
+    openAndCloseItemMenuBurger()
+  })
+})
+
+function changeSizeDisplay() {
+  const screenWidth = window.innerWidth;
+  if (screenWidth > 768) {
+    openAndCloseItemMenuBurger()
+  }
+}
+
+window.addEventListener('resize', changeSizeDisplay)
